2dfda918b6e284e783667e1c41ddb4f302a5ca05
Aleem547 <abdulaleem5470@gmail.com>
2026-03-27 16:21:26 +0530

    JS

---
+++ app.js
@@ -0,0 +1,67 @@
+// let num=5;
+// let newnum=num++;
+// console.log(num);
+// console.log(newnum);
+// console.log("Hello world");
+// let a=10;
+// let b=5;
+// console.log("Sum of a and b is :", a+b);
+// // Arthmetic operator
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// //unary operator
+// console.log(a++);
+// console.log(++a);
+// //assignment operator
+// b=a;
+// console.log(b);
+
+// let age=18;
+// console.log(age>=18);
+// //conditional operator
+// //Traffic light System
+// let color="red";
+// if (color=="red"){
+//     console.log("Stop!");
+// }
+// else if(color=="green"){
+//     console.log("Go");
+// }    
+// else if(color=="yellow"){
+//     console.log("Go faster");
+// }
+
+// let age=14;
+// if(age>=18){
+//     console.log("You are eligible to vote");    
+// }else if (age>=18){
+//     console.log("You are eligible to vote");
+// }else if(age<=18){
+//     console.log("You are not eligible to vote");
+// }
+
+// let marks=74;
+// if (marks>=90){
+//     console.log("Grade A");
+// }else if(marks>=80){
+//     console.log("grade B");
+// }else if(marks>=70){
+//     console.log("Grade C");
+// }else if(marks>=60){
+//     console.log("Fail");
+// } 
+
+// let month ="January";
+// if(month==="January"){
+//     console.log("Winter");
+// }else if (month==="May"){
+//     console.log("Summer");
+// }else if (month==="September"){
+//     console.log("Autumn");
+// }
+ 
+let age = 17;
