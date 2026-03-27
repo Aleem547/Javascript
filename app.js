94e0e24ddfeabb0688f5690aa9fdf07407ae9ede
Aleem547 <abdulaleem5470@gmail.com>
2026-03-27 18:25:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,7 @@
 // b=a;
 // console.log(b);
 
+
 // let age=18;
 // console.log(age>=18);
 // //conditional operator
@@ -63,5 +64,135 @@
 // }else if (month==="September"){
 //     console.log("Autumn");
 // }
- 
-let age = 17;
+
+// let age = 18;
+// if(age>=18){
+//     console.log("You are eligible to vote"); 
+// }else{
+//     console.log("You are not eligible to vote");
+// }
+
+// let color="red";
+
+// if(color==="red"){
+//     console.log("Stop!");
+// }else if(color==="green"){
+//     console.log("go");
+// }else if(color==="yellow"){
+//     console.log("go faster");
+// }else{
+//     console.log("Light is Broken");
+// }
+
+// let size="S";
+
+// if(size==="XL"){
+//     console.log("Price is Rs 250");
+// }else if (size==="L"){
+//     console.log("Price is Rs 200");
+// }else if (size==="M"){
+//     console.log("price is Rs 100");    
+// }else if (size==="S"){
+//     console.log("Price is Rs 50");    
+// }else{
+//     console.log("Popcorn is not available");
+// }
+
+//NESTED LOOPS
+// let marks=90;
+// if(marks>=33){
+//     console.log("PASS");
+//     if (marks>=80){
+//         console.log("O");
+//     }else{
+//         console.log("Grade A")
+//     }
+// }else{
+//     console.log("FAIL");
+// }
+
+//Logical operator
+//And operator
+// let marks=90;
+// if(marks>=33 && marks>=80){
+//     console.log("PASS");
+//     console.log("A+");
+// }
+
+//OR Operator,Pipe
+// let marks=90;
+// if(marks>=33 || marks>=80){
+//     console.log("PASS");
+//     console.log("A+");
+// }
+
+//Logical Not(!)
+// let marks=90;
+// if(!(marks<33)){
+//     console.log("PASS");
+//     console.log("A+");
+// } 
+
+// let marks=75;
+// if((marks>33 && marks>=80)||!false){
+//     console.log("Pass"); 
+// }
+
+// let str="apple";
+// if((str[0]==='a')&&(str.length>3)){
+//     console.log("good string");
+// }else{
+//     console.log("not a good string");
+// }
+
+// let color = "r";
+// switch (color) {
+//     case "red":
+//         console.log("Stop");
+//         break;
+//     case "green":
+//         console.log("Go");
+//         break;
+//     case "yellow":
+//         console.log("Go Faster");
+//         break;
+//     default:
+//         console.log("Broken light ")
+//         break;
+// }
+
+//     console.log("after the stt ");
+
+// let day=1;
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;  
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;    
+//     default:
+//         console.log("NO day");
+//         break;
+
+// }
+//Alert
+// alert("Everything is wrong");
+//Prompt
+// let firstName= prompt("Enter your name :");
+// console.log(firstName); 
