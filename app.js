b3e8f000e970ba319b55978d363eecc211370247
Aleem547 <abdulaleem5470@gmail.com>
2026-03-28 14:36:15 +0530

    Array

--- app.js
+++ app.js
@@ -196,3 +196,5 @@
 //Prompt
 // let firstName= prompt("Enter your name :");
 // console.log(firstName); 
+
+
---
+++ stringsandarray.js
@@ -0,0 +1,36 @@
+// let msg="   hello   ";
+//msg.trim()
+
+// let password=prompt("set password");
+// let newPass =password.trim();  it creates the new string for removing the space, it doesn't chnge the old string, string is immutable in js 
+// console.log(newPass); 
+
+//method chaining
+// Msg="            hello  "
+// let newMsg=Msg.trim().toUpperCase();
+// console.log(newMsg);
+
+//slice method
+ 
+// let msg="abdulansari";
+// newmsg=msg.replace("ansari","aleem")
+// newmsg=msg.repeat(10);
+
+// let msg="     help!";
+// newMsg=msg.trim().toUpperCase();
+
+//ARRAYS
+
+// let student1="abdul";
+// let student2="aleem";
+// let student3="ansari";
+
+// let students=["abdul","aleem","ansari"]
+
+// let months=["January","July","march","august"];
+// months.shift();
+// months.shift();
+// months.unshift("june");
+// months.unshift("july");
+
+
