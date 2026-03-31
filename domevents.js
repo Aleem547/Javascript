b2237701c230fb443c66a99d913f8918bbbedbc6
Aleem547 <abdulaleem5470@gmail.com>
2026-03-31 18:29:04 +0530

    Dom events

---
+++ domevents.js
@@ -0,0 +1,38 @@
+//onclick
+// let btns=document.querySelectorAll("button");
+
+// for(btn of btns){
+//     btn.onclick=sayHello;
+// }
+
+// function sayHello(){
+//     alert("Hello"); 
+// }
+//onmouseenter
+// let btns=document.querySelectorAll("button");
+
+// for(btn of btns){
+//     btn.onclick=sayHello;
+//     btn.onmouseenter=function(){
+//         console.log("you entered a button");
+//     };
+//     console.log(dir);
+// }
+// function sayHello(){
+//     alert("Hello"); 
+// }
+
+//addeventlistner
+// let btns=document.querySelectorAll("button");
+// for(btn of btns){
+//     btn.addEventListener("click",sayHello);
+//     btn.addEventListener("click",sayName);
+// }
+// function sayHello(){
+//     alert("Hello"); 
+// }
+// function sayName(){
+//     alert("Some");
+// }
+
+//Example
